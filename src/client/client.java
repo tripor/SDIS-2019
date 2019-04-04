6f8a2504a28d06005cd9c5d6698c50d0f3a512fe
up201608320 <henriqinho20@gmail.com>
2019-04-04 02:54:33 +0100

    Started implementing RMI and client interface for the testApp

--- src/client/client.java
+++ src/client/client.java
@@ -1,6 +1,25 @@
+import java.io.*;
+import java.util.*;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+import server.ClientInterface;
+
 class Client
 {
     public static void main(String[] args) {
         System.out.println("ola from client");
+
+        try {
+            Registry registry = LocateRegistry.getRegistry();
+            ClientInterface stub = (ClientInterface) registry.lookup(args[0]);
+            //String response = stub.request(algo);
+            //System.out.println(fstr + " : " + response);
+        } catch (Exception e) {
+            //System.out.println("\n" + fstr + " : EXCEPTION_ERROR");
+            System.err.println("Client exception: " + e.toString());
+            e.printStackTrace();
+        }
     }
+     
 }
---
+++ src/server/ClientInterface.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+import java.rmi.registry.Registry;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.RemoteException;
+import java.rmi.Remote;
+import java.rmi.server.UnicastRemoteObject;
+
+public interface ClientInterface extends Remote {
+    String request(String opr) throws RemoteException;
+}
+
+/* 
+* Class where the client interface will be implemented
+* DBS-distributed backup service
+*/
+public class DBS implements ClientInterface {
+    public DBS() {}
+
+    public String request(String opr) {
+
+        System.out.println("Server processing message: " + opr + "...");
+    }
+
+}
